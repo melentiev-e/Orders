f8f9f1094f6abb48c0bf9b959a964f568452951e
Yevhen Melentiev <melentiev.91@gmail.com>
2022-07-03 13:22:07 +0300

    base part for domain entities was added

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    private readonly List<BaseEvent> _domainEvents = new();
+    
+    public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(BaseEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+}
